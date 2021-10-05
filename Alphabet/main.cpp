8f438aa2856dc5d109b31d2e5be204ba7e0a468f
Герман <winami5582@soulsuns.com>
2021-10-05 14:05:58 +0300

    added a

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,10 +2,19 @@
 #include <array>
 
 using namespace std;
+void a(array<string, 6>&s){
+    s[0]+="         ";
+    s[1]+="         ";
+    s[2]+="   /\\   ";
+    s[3]+="  /__\\  ";
+    s[4]+=" /    \\ ";
+    s[5]+="||    ||";
+}
 
-int main()
-{
+int main(){
         array < string, 6 > screen;
+        a(screen);
+        a(screen);
 
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
