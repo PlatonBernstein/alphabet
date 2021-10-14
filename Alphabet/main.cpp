5847a5a82388e9ecc3695b0d2c8101903801990f
KOSMOGOR <mortalmortal2015@mail.ru>
2021-10-14 14:47:33 +0300

    added e

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,21 +2,21 @@
 #include <array>
 
 using namespace std;
-void h(array<string, 6> &s) {
-    s[0] += "|       ";
-    s[1] += "|       ";
-    s[2] += "|______ ";
-    s[3] += "|      \\";
-    s[4] += "|      |";
-    s[5] += "|      |";
+void e(array<string, 6> &s) {
+    s[0] += "        ";
+    s[1] += " ______ ";
+    s[2] += "/      \\";
+    s[3] += "|______/";
+    s[4] += "\\       ";
+    s[5] += " \\_____ ";
 }
 
 
 int main()
 {
     array<string, 6> screen;
-    h(screen);
-    h(screen);
+    e(screen);
+    e(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
