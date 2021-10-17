1cac4cd24363a904c2be75a3dc2c23cfee544b09
Mad <1233@gmail.com>
2021-10-17 22:51:38 +0400

    Added L

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,11 +2,19 @@
 #include <array>
 
 using namespace std;
-
+void l(array<string, 6>& s) {
+    s[0] += "      ";
+    s[1] += "||    ";
+    s[2] += "||    ";
+    s[3] += "||    ";
+    s[4] += "||    ";
+    s[5] += "||____";
+}
 int main()
 {
         array < string, 6 > screen;
-
+    l(screen);
+    l(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
