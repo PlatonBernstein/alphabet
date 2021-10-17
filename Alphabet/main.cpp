f7231698afa84e3602b7937bbb9484dee746728b
KOSMOGOR <mortalmortal2015@mail.ru>
2021-10-17 12:57:57 +0300

    added n

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,21 +2,21 @@
 #include <array>
 
 using namespace std;
-void k(array<string, 6> &s) {
-    s[0] += "|       ";
-    s[1] += "|  /    ";
-    s[2] += "| /     ";
-    s[3] += "|-      ";
-    s[4] += "| \\     ";
-    s[5] += "|  \\    ";
+void n(array<string, 6> &s) {
+    s[0] += "        ";
+    s[1] += "  ____  ";
+    s[2] += "|/    \\ ";
+    s[3] += "|      |";
+    s[4] += "|      |";
+    s[5] += "|      |";
 }
 
 
 int main()
 {
     array<string, 6> screen;
-    k(screen);
-    k(screen);
+    n(screen);
+    n(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
