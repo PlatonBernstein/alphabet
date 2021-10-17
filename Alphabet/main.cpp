9317c6a416e2a632f81c4e9b9a72c9f7640155f2
Mad <1233@gmail.com>
2021-10-17 22:34:37 +0400

    Added f

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,10 +2,19 @@
 #include <array>
 
 using namespace std;
-
+void f(array<string, 6>& s) {
+    s[0] += "      ";
+    s[1] += "   ___";
+    s[2] += "  |   ";
+    s[3] += "__|__ ";
+    s[4] += "  |   ";
+    s[5] += "  |   ";
+}
 int main()
 {
         array < string, 6 > screen;
+    f(screen);
+    f(screen);
 
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
