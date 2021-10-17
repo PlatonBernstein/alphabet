6136b0f054d0b5e20a0028edd04130a5add40a4d
KOSMOGOR <mortalmortal2015@mail.ru>
2021-10-17 12:55:09 +0300

    added k

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,21 +2,21 @@
 #include <array>
 
 using namespace std;
-void e(array<string, 6> &s) {
-    s[0] += "        ";
-    s[1] += " ______ ";
-    s[2] += "/      \\";
-    s[3] += "|______/";
-    s[4] += "\\       ";
-    s[5] += " \\_____ ";
+void k(array<string, 6> &s) {
+    s[0] += "|       ";
+    s[1] += "|  /    ";
+    s[2] += "| /     ";
+    s[3] += "|-      ";
+    s[4] += "| \\     ";
+    s[5] += "|  \\    ";
 }
 
 
 int main()
 {
     array<string, 6> screen;
-    e(screen);
-    e(screen);
+    k(screen);
+    k(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
