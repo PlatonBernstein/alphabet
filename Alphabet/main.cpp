e5463d06ea2a269be7180941d27cc3dffc825606
Mad <1233@gmail.com>
2021-10-17 23:02:28 +0400

    Added O

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,10 +2,19 @@
 #include <array>
 
 using namespace std;
-
+void o(array<string, 6>& s) {
+    s[0] += "        ";
+    s[1] += "  ____  ";
+    s[2] += " /    \\ ";
+    s[3] += "|      |";
+    s[4] += "|      |";
+    s[5] += " \\____/ ";
+}
 int main()
 {
         array < string, 6 > screen;
+    o(screen);
+    o(screen);
 
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
