d329e720701771e48acef4e0c8f618cc47e3dd5b
KOSMOGOR <mortalmortal2015@mail.ru>
2021-10-17 13:34:10 +0300

    added t

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,31 +2,21 @@
 #include <array>
 
 using namespace std;
-void a(array<string, 6>&s){
-    s[0]+="         ";
-    s[1]+="         ";
-    s[2]+="   /\\   ";
-    s[3]+="  /__\\  ";
-    s[4]+=" /    \\ ";
-    s[5]+="||    ||";
-}
-void b(array<string, 6> &s) {
-    s[0] += "|       ";
-    s[1] += "|       ";
-    s[2] += "|______ ";
-    s[3] += "|      \\";
-    s[4] += "|      |";
-    s[5] += "|\\_____/";
+void t(array<string, 6> &s) {
+    s[0] += "  |     ";
+    s[1] += "__|__   ";
+    s[2] += "  |     ";
+    s[3] += "  |     ";
+    s[4] += "  |     ";
+    s[5] += "  \\___/ ";
 }
 
 
 int main()
 {
     array<string, 6> screen;
-    a(screen);
-    a(screen);
-    b(screen);
-    b(screen);
+    t(screen);
+    t(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
