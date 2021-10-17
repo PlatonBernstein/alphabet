385ed73b2edc228af42e19cb090b1c1ff312f7a1
Mad <1233@gmail.com>
2021-10-17 23:09:09 +0400

    Added R

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,10 +2,20 @@
 #include <array>
 
 using namespace std;
+void r(array<string, 6>& s) {
+    s[0] += " ____ ";
+    s[1] += "||  //";
+    s[2] += "|| // ";
+    s[3] += "||//  ";
+    s[4] += "||\\   ";
+    s[5] += "|| \\  ";
+}
 
 int main()
 {
         array < string, 6 > screen;
+    r(screen);
+    r(screen);
 
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
