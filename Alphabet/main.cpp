6378a885d0ecaf15281fc9904c97e8b18b8d5869
KOSMOGOR <mortalmortal2015@mail.ru>
2021-10-17 13:27:11 +0300

    added q

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,31 +2,21 @@
 #include <array>
 
 using namespace std;
-void a(array<string, 6>&s){
-    s[0]+="         ";
-    s[1]+="         ";
-    s[2]+="   /\\   ";
-    s[3]+="  /__\\  ";
-    s[4]+=" /    \\ ";
-    s[5]+="||    ||";
-}
-void b(array<string, 6> &s) {
-    s[0] += "|       ";
-    s[1] += "|       ";
-    s[2] += "|______ ";
-    s[3] += "|      \\";
+void q(array<string, 6> &s) {
+    s[0] += "        ";
+    s[1] += "  ____  ";
+    s[2] += " /    \\ ";
+    s[3] += "|      |";
     s[4] += "|      |";
-    s[5] += "|\\_____/";
+    s[5] += " \\____/\\";
 }
 
 
 int main()
 {
     array<string, 6> screen;
-    a(screen);
-    a(screen);
-    b(screen);
-    b(screen);
+    q(screen);
+    q(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
