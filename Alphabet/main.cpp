af2b332afbaadb0b5e8f16139c0d15b5feaf99fa
Mad <1233@gmail.com>
2021-10-17 23:20:11 +0400

    Added u

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,10 +2,19 @@
 #include <array>
 
 using namespace std;
-
+void u(array<string, 6>& s) {
+    s[0] += "      ";
+    s[1] += "|    |";
+    s[2] += "|    |";
+    s[3] += "|    |";
+    s[4] += "|    |";
+    s[5] += " \\__/|";
+}
 int main()
 {
-        array < string, 6 > screen;
+        array < string,6 > screen;
+    u(screen);
+    u(screen);
 
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
