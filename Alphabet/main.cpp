0b8ffaa930847b609a0bf050e9a81264d281d1e3
Joman <German.Vlasenko@bk.ru>
2021-10-18 16:50:12 +0300

    added j

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,11 +2,20 @@
 #include <array>
 
 using namespace std;
+void j(array<string, 6> &s) {
+    s[0] += "      ()";
+    s[1] += "        ";
+    s[2] += "      ||";
+    s[3] += "      ||";
+    s[4] += "      ||";
+    s[5] += "\\\\====//";
+}
 
 int main()
 {
         array < string, 6 > screen;
-
+    j(screen);
+    j(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
