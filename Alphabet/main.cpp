a354dc387370dcbe4ffe6d9fee057201ec98497b
Joman <German.Vlasenko@bk.ru>
2021-10-18 16:49:03 +0300

    added g

--- Alphabet/main.cpp
+++ Alphabet/main.cpp
@@ -2,11 +2,20 @@
 #include <array>
 
 using namespace std;
+void g(array<string, 6> &s) {
+    s[0] += " ______|";
+    s[1] += "/      |";
+    s[2] += "|      |";
+    s[3] += "\\______|";
+    s[4] += "       |";
+    s[5] += "\\______/";
+}
 
 int main()
 {
         array < string, 6 > screen;
-
+    g(screen);
+    g(screen);
     for(int i = 0; i < 6; i++)
         cout << screen[i] << endl;
     return 0;
